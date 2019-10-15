a508ba3c658197f68cd821e59e512ef4fe3511af
Bonaventura Jason <jason@lubna.io>
2019-10-15 15:39:57 +0700

    Implement BarChart and LineChart message

--- app/src/main/java/com/example/chartirvan/activity/BarChartActivity.java
+++ app/src/main/java/com/example/chartirvan/activity/BarChartActivity.java
@@ -1,13 +1,26 @@
 package com.example.chartirvan.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.Log;
 
 import com.example.chartirvan.R;
+import com.example.chartirvan.model.Malware;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class BarChartActivity extends AppCompatActivity {
+    private static final String TAG = "BarChartActivity";
+    private BarChart mChart;
+    private ArrayList<Malware>mDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +28,43 @@ public class BarChartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bar_chart);
 
         // Get the intent and its data.
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.bar_textview);
-        textView.setText(message);
+        String malwareString = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Gson gson = new Gson();// create object GSON untuk rubah lagi ke object Malware
+        Type type = new TypeToken<ArrayList<Malware>>(){}.getType();
+        mDataList = gson.fromJson(malwareString, type); //Object dimasukkan ke dalam arraylist lagi
+        for( Malware malware : mDataList){
+            Log.d(TAG, "onCreate:"+malware);
+        }
+
+        mChart = findViewById(R.id.barChart);
+        mChart.getDescription().setEnabled(false);
+
+        setData(mDataList.size());
+        mChart.setFitBars(true);
+
+    }
+
+    private void setData(int size) {
+        ArrayList<BarEntry> yVals = new ArrayList<>(); //Array List untuk BarEntry
+
+
+
+        for(int i = 0; i < size; i++){
+            float y = (float)mDataList.get(i).getFlowPackets();
+
+            yVals.add(new BarEntry(i,y));
+
+            Log.d(TAG, "setData: " + yVals);
+        } // dibuat x.y nya dengan variable name yVals, di set sejumlah panjang ArrayList dengan Data Type Malware
+
+        BarDataSet set = new BarDataSet(yVals, "Data Set");
+        set.setColors(ColorTemplate.MATERIAL_COLORS);
+        set.setDrawValues(true);
+
+        BarData data = new BarData(set);
+
+        mChart.setData(data);
+        mChart.invalidate();
+        mChart.animateX(500);
     }
 }
--- app/src/main/java/com/example/chartirvan/activity/HeatMapChartActivity.java
+++ app/src/main/java/com/example/chartirvan/activity/HeatMapChartActivity.java
@@ -1,15 +1,65 @@
 package com.example.chartirvan.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.chartirvan.R;
+import com.example.chartirvan.model.Malware;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class HeatMapChartActivity extends AppCompatActivity {
-
+    private static final String TAG = "HeatMapChartActivity";
+    private LineChart mChart;
+    private ArrayList<Malware> mDataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heat_map_chart);
+
+        String malwareString = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Gson gson = new Gson();// create object GSON untuk rubah lagi ke object Malware
+        Type type = new TypeToken<ArrayList<Malware>>(){}.getType();
+        mDataList = gson.fromJson(malwareString, type); //Object dimasukkan ke dalam arraylist lagi
+
+        mChart = findViewById(R.id.lineChart);
+
+//        mChart.setOnChartGestureListener(HeatMapChartActivity.this);
+//        mChart.setOnChartValueSelectedListener(HeatMapChartActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+
+
+
+        setData(mDataList.size());
+
+    }
+
+    private void setData(int size) {
+        ArrayList<Entry> yValues = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            float y = (float)mDataList.get(i).getFwdPackets();
+            yValues.add(new Entry(i, y));
+        }
+
+        LineDataSet set  = new LineDataSet(yValues, "Data Set");
+        set.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
+
     }
 }
